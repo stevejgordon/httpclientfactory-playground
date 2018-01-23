446e0a1bcb9c573fb37037ad821a7f61d7777cbf
Steve Gordon <sgordon@hotmail.co.uk>
2018-01-23 19:14:53 +0000

    Initial

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientFactoryTesting.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var result = await client.GetStringAsync("http://www.google.com");
+
+            var client2 = _httpClientFactory.CreateClient("Name"); // asking for a named client
+
+            var result2 = await client.GetStringAsync("/"); // will add this path to the base url
+
+            return Ok(result);
+        }
+    }
+
+    [Route("api/[controller]")]
+    public class AnotherController : Controller
+    {
+        private readonly SomeService _someService;
+
+        public AnotherController(SomeService service)
+        {
+            _someService = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var result = await _someService.GetRawData();
+
+            return Ok(result);
+        }
+    }
+
+    public class SomeService
+    {
+        private readonly HttpClient _client;
+
+        public SomeService(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("http://www.google.com");
+        }
+
+        public async Task<string> GetRawData()
+        {
+            return await _client.GetStringAsync("");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HttpClientFactoryTesting.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HttpClientFactoryTesting
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+
+            services.AddHttpClient("Name", config => {
+               config.BaseAddress = new Uri("http://www.google.com");
+            });
+
+            services.AddHttpClient<SomeService>();
+
+            services.AddHttpClient<SomeService>().AddHttpMessageHandler<RetryHandler>();
+            // what this is effectively middleware for the outogoing request
+            // if we run this the retry handler will be called first before the actual handler which will send the request.
+            // you could wrap the sendAsync and catch an exception to attempt a retry for example
+
+            services.AddMvc();
+        }
+
+        public class RetryHandler : DelegatingHandler
+        {
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken)
+            {
+                return base.SendAsync(requestMessage, cancellationToken);
+            }   
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
